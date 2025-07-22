f403ab857b59797a86c6be566dc28e9152159bc6
Max <m.nielsen@uke.de>
2025-07-22 12:17:01 +0200

    wildheit

--- baseline-algorithm/model.py
+++ baseline-algorithm/model.py
@@ -7,19 +7,21 @@ The file must contain a function called `run_algorithm` that takes two arguments
 """
 from pathlib import Path
 import numpy as np
+from PIL import Image
 
+from dam4sam.dam4sam_tracker import DAM4SAMTracker
 RESOURCE_PATH = Path("resources")  # load weights and other resources from this directory
 
 
 def run_algorithm(frames: np.ndarray, target: np.ndarray, frame_rate: float, magnetic_field_strength: float, scanned_region: str) -> np.ndarray:
-    tracker = DAM4SAMTracker('sam21pp-L')
+    tracker = DAM4SAMTracker('sam21pp-L', base_path="/opt/ml/model/")
     initial_mask = target[..., 0].astype(bool)
     frames = ((frames - frames.min()) / (frames.max() - frames.min())) * 255
     frames = frames.astype(np.uint8)
     initial_frame = Image.fromarray(np.repeat(frames[..., 0:1], 3, -1), mode='RGB')
     tracker.initialize(initial_frame, initial_mask)
     tracked_masks = [initial_mask]
-    for i in trange(1, frames.shape[-1]):
+    for i in range(1, frames.shape[-1]):
         frame = Image.fromarray(np.repeat(frames[..., i:i+1], 3, -1), mode='RGB')
         tracked_masks.append(tracker.track(frame)['pred_mask'])
     tracked_masks = np.stack(tracked_masks, axis=-1)
