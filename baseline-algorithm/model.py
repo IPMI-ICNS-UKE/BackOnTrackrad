77c865f1634774a29da99594f41b5d8b9887133d
Max <m.nielsen@uke.de>
2025-07-22 11:30:53 +0200

    initi 2

---
+++ baseline-algorithm/model.py
@@ -0,0 +1,45 @@
+"""
+Edit this file to implement your algorithm. 
+
+The file must contain a function called `run_algorithm` that takes two arguments:
+- `frames` (numpy.ndarray): A 3D numpy array of shape (W, H, T) containing the MRI linac series.
+- `target` (numpy.ndarray): A 2D numpy array of shape (W, H, 1) containing the MRI linac target.
+"""
+from pathlib import Path
+import numpy as np
+
+RESOURCE_PATH = Path("resources")  # load weights and other resources from this directory
+
+
+def run_algorithm(frames: np.ndarray, target: np.ndarray, frame_rate: float, magnetic_field_strength: float, scanned_region: str) -> np.ndarray:
+    tracker = DAM4SAMTracker('sam21pp-L')
+    initial_mask = target[..., 0].astype(bool)
+    frames = ((frames - frames.min()) / (frames.max() - frames.min())) * 255
+    frames = frames.astype(np.uint8)
+    initial_frame = Image.fromarray(np.repeat(frames[..., 0:1], 3, -1), mode='RGB')
+    tracker.initialize(initial_frame, initial_mask)
+    tracked_masks = [initial_mask]
+    for i in trange(1, frames.shape[-1]):
+        frame = Image.fromarray(np.repeat(frames[..., i:i+1], 3, -1), mode='RGB')
+        tracked_masks.append(tracker.track(frame)['pred_mask'])
+    tracked_masks = np.stack(tracked_masks, axis=-1)
+
+    """
+    Implement your algorithm here.
+
+    Args:
+    - frames (numpy.ndarray): A 3D numpy array of shape (W, H, T) containing the MRI linac series.
+    - target (numpy.ndarray): A 2D numpy array of shape (W, H, 1) containing the MRI linac target.
+    - frame_rate (float): The frame rate of the MRI linac series.
+    - magnetic_field_strength (float): The magnetic field strength of the MRI linac series.
+    - scanned_region (str): The scanned region of the MRI linac series.
+    """
+    
+    # frames.shape == (W, H, T)
+    # target.shape == (W, H, 1)
+
+    # For the example we want to repeat the initial segmentation for every frame 
+    # repeated_target = np.repeat(target, frames.shape[2], axis=-1)
+
+    # repeated_target.shape == (W, H, T)
+    return tracked_masks
